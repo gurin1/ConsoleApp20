002179a36d2ad7e545931f3c80a000f3d787d424
Хвастунов <op.ap.191@mail.ru>
2022-12-05 12:28:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите 1е число: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите 2е число: ");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"{x}+{y}={x + y}");
+Console.WriteLine($"{x}-{y}={x - y}");
+Console.WriteLine($"{x}*{y}={x * y}");
+Console.WriteLine($"{x}/{y}={Convert.ToDouble(x / y)}");
